d0125931687c3056f04eec9b50a83aad9327d4e7
Boogu <boogunote@gmail.com>
2015-04-08 00:15:43 +0800

    [Feature] Add window and titlebar

--- src/flat.js
+++ src/flat.js
@@ -65,43 +65,79 @@ export class Tree{
   }
 
   initInteract(id) {
+    // interact('#'+id)
+    //   .draggable({
+    //     // enable inertial throwing
+    //     inertia: true,
+    //     // keep the element within the area of it's parent
+    //     restrict: {
+    //       restriction: "parent",
+    //       endOnly: true,
+    //       elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    //     },
+
+    //     // call this function on every dragmove event
+    //     onmove: function (event) {
+    //       var target = event.target,
+    //           // keep the dragged position in the data-x/data-y attributes
+    //           x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    //           y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    //       // translate the element
+    //       target.style.webkitTransform =
+    //       target.style.transform =
+    //         'translate(' + x + 'px, ' + y + 'px)';
+
+    //       // update the posiion attributes
+    //       target.setAttribute('data-x', x);
+    //       target.setAttribute('data-y', y);
+    //       console.log(x + " " + y)
+    //     },
+    //     // call this function on every dragend event
+    //     onend: function (event) {
+    //       var textEl = event.target.querySelector('p');
+
+    //       textEl && (textEl.textContent =
+    //         'moved a distance of '
+    //         + (Math.sqrt(event.dx * event.dx +
+    //                      event.dy * event.dy)|0) + 'px');
+    //     }
+    //   });
+    interact('#'+id+" .flat-body")
+      .resizable({
+        edges: {
+          left: true,
+          right: true,
+          bottom: true,
+          top: false
+        },
+        square: true
+      })
+      .on("resizemove", function(event) {
+        var target = event.target.parentElement;
+        var dRect = event.deltaRect;
+        var newWidth  = parseFloat(target.style.width ) + dRect.width;
+        var newHeight = parseFloat(target.style.height) + dRect.height;
+        var x = (parseFloat(target.getAttribute('data-x')) || 0) + dRect.left;
+        var y = (parseFloat(target.getAttribute('data-y')) || 0) + dRect.top;
+        // update the element's style
+        target.style.width  = newWidth + 'px';
+        target.style.height = newHeight + 'px';
+        target.style.transform = "translate(" + x + "px, " + y + "px)";
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+      })
+    // interact('#'+id+" .flat-window")
     interact('#'+id)
-      .draggable({
-        // enable inertial throwing
-        inertia: true,
-        // keep the element within the area of it's parent
-        restrict: {
-          restriction: "parent",
-          endOnly: true,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
-
-        // call this function on every dragmove event
-        onmove: function (event) {
-          var target = event.target,
-              // keep the dragged position in the data-x/data-y attributes
-              x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-              y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-          // translate the element
-          target.style.webkitTransform =
-          target.style.transform =
-            'translate(' + x + 'px, ' + y + 'px)';
-
-          // update the posiion attributes
-          target.setAttribute('data-x', x);
-          target.setAttribute('data-y', y);
-          console.log(x + " " + y)
-        },
-        // call this function on every dragend event
-        onend: function (event) {
-          var textEl = event.target.querySelector('p');
-
-          textEl && (textEl.textContent =
-            'moved a distance of '
-            + (Math.sqrt(event.dx * event.dx +
-                         event.dy * event.dy)|0) + 'px');
-        }
-      });
+      .draggable({})
+      .allowFrom(".flat-titlebar")
+      .on("dragmove", function(event) {
+        var target = event.target;
+        var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+        var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        target.style.transform = "translate(" + x + "px, " + y + "px)";
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+      })
   }
 }
